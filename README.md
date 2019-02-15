b22325fe012ac055adac409edc9750b562eed2a9
ranjayzheng <ranjayzheng@qq.com>
2019-02-15 16:30:34 +0800

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # react-ssr-scaffold
 👷React SSR 脚手架
+
+## SSR基本流程：
+0. （匹配前端传来路由对应的组件、初始化状态、执行getInitialProps）生成完整组件
+1. 服务端渲染 HTML String 直出
+2. 浏览器解析 HTML
+3. 执行前端 bundle JS
+4. JS 中的 React 代码接管页面操作（路由，Redux）
--- src/client/index.js
+++ src/client/index.js
@@ -2,5 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Hello from '../components/Hello/Hello';
 
-// 只绑定事件，不渲染组件
+// 如果服务端直出，则只绑定事件，不渲染组件
 ReactDOM.hydrate(<Hello />, document.getElementById('root'))

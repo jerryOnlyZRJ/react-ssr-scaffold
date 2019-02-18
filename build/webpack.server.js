7e8208959ae01f63aa7e4e8f05d8b293e9c3347d
ranjayzheng <ranjayzheng@qq.com>
2019-02-18 16:41:04 +0800

    optimize output file's name

--- build/webpack.server.js
+++ build/webpack.server.js
@@ -8,6 +8,7 @@ module.exports = {
     output: {
         path: path.join(__dirname, "../dist/server"),
         filename: "app.js",
+        chunkFilename: "[name].chunk.js",
     },
     module: {
         rules: [{
